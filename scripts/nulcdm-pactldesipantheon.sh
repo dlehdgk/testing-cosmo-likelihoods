687234844b69f567fbdaf4c764b43fe53cd32b34
DHL-Sheffield <dhlee1@sheffield.ac.uk>
2025-06-09 10:07:03 +0100

    nulcdm-pactldesipantheon converged

--- scripts/nulcdm-pactldesipantheon.sh
+++ scripts/nulcdm-pactldesipantheon.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks-per-node=4
 #SBATCH --cpus-per-task=2
 #SBATCH --time=96:00:00
-#SBATCH --mem=30G
+#SBATCH --mem=36G
 #SBATCH --mail-type=fail,end
 #SBATCH --mail-user=dhlee1@sheffield.ac.uk
 
